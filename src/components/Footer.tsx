7a797980f00362759cd41fddf0ecdd062bbf1f3b
rajaitservice <suhash.raja@rajaitservice.com>
2025-09-30 16:16:37 +0200

    Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -34,7 +34,7 @@ const Footer = () => {
             </div>
           </div>
           
-          <div>
+          {/*<div>
             <h3 className="text-xl font-bold mb-6">Company</h3>
             <ul className="space-y-3">
               <li>
@@ -53,7 +53,7 @@ const Footer = () => {
                 </a>
               </li>
             </ul>
-          </div>
+          </div>*/}
         </div>
         
         <div className="border-t border-gray-700 pt-8 text-center">
